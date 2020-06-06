8a019949d3449261a4b9cf8dadb3d3666896044e
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 10:48:04 +0530

    Complete Problem "Check burgs"

--- Problems/Check burgs/src/Main.java
+++ Problems/Check burgs/src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine();
-        inputString = inputString.replace('a', 'b');
-        System.out.println(inputString);
+        System.out.println(inputString.endsWith("burg"));
     }
 }
