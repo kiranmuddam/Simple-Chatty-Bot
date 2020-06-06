26f3a9499507f2bab4d1000af566a36832646977
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 16:52:08 +0530

    Complete problem "The integer barrier".

---
+++ Problems/The integer barrier/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int currentNumber = scanner.nextInt();
+        int totalSum = 0;
+        while (scanner.hasNext()) {
+            totalSum = totalSum + currentNumber;
+            if (currentNumber == 0) {
+                System.out.println(totalSum);
+                break;
+            } else if (totalSum >= 1000) {
+                System.out.println(totalSum - 1000);
+                break;
+            }
+            currentNumber = scanner.nextInt();
+        }
+    }
+}
