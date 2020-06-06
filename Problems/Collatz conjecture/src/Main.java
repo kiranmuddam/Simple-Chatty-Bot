36382aa0567d7eaa8e8dbd6a591d520f0dcba2f9
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 16:14:55 +0530

    Complete problem "Collatz conjecture".

---
+++ Problems/Collatz conjecture/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNum = scanner.nextInt();
+        while (inputNum != 1) {
+            System.out.print(inputNum + " ");
+            if (inputNum % 2 == 0) {
+                inputNum = inputNum / 2;
+            } else if (inputNum % 2 == 1) {
+                inputNum = inputNum * 3 + 1;
+            }
+        }
+        System.out.println(1);
+    }
+}
