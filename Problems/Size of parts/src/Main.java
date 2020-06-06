a39767a4b7de13f2aa3ee82dab83aea41f348282
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 13:53:50 +0530

    complete problem "Size of Parts"

---
+++ Problems/Size of parts/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalPartsCount = scanner.nextInt();
+        int fixedPartsCount = 0;
+        int rejectedPartsCount = 0;
+        int perfectPartsCount = 0;
+        for (int i = 0; i < totalPartsCount; ++i) {
+            int currentPart = scanner.nextInt();
+            if (currentPart == 1) {
+                ++fixedPartsCount;
+            } else if (currentPart == -1) {
+                ++rejectedPartsCount;
+            } else {
+                ++perfectPartsCount;
+            }
+        }
+        System.out.println(perfectPartsCount + " " + fixedPartsCount + " " + rejectedPartsCount);
+    }
+}
