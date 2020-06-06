a08cebf4ef73ec0c8dd0f91d31297bd2c397eef5
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 11:24:10 +0530

    fix code style problem "Check the value"

--- Problems/Check the value/src/Main.java
+++ Problems/Check the value/src/Main.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int inputNum = scanner.nextInt();
-        boolean result = (inputNum > 0) && (inputNum < 10);
+        boolean result = inputNum > 0 && inputNum < 10;
         System.out.println(result);
     }
 }
