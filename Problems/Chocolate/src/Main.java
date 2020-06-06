0eade1b180df6facccc6be97a58a2f127b79b512
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 13:17:41 +0530

    complete problem "Chocolate"

---
+++ Problems/Chocolate/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int width = scanner.nextInt();
+        int segments = scanner.nextInt();
+        if (length * width < segments) {
+            System.out.println("NO");
+        } else if (segments % length == 0 || segments % width == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
