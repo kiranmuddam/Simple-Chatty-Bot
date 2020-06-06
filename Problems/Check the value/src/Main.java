946932240f03d34236bcf49d9ffa80d82d97ab2a
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 11:22:31 +0530

    Complete problem "Check the value"

---
+++ Problems/Check the value/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNum = scanner.nextInt();
+        boolean result = (inputNum > 0) && (inputNum < 10);
+        System.out.println(result);
+    }
+}
