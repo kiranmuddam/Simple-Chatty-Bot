c515910f260589976eca47674a66e25c86f0f824
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 16:41:54 +0530

    Complete problem "Even or odd".

---
+++ Problems/Even or odd/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int currentNumber = scanner.nextInt();
+        while (currentNumber != 0) {
+            if (currentNumber % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+            currentNumber = scanner.nextInt();
+        }
+    }
+}
