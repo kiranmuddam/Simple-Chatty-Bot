c8bfceaf6d49ac35e2919ddd462a6ef475b2b4e6
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 10:55:04 +0530

    Complete Problem "Comparing strings ignoring whitespaces"

---
+++ Problems/Comparing strings ignoring whitespaces/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstLine = scanner.nextLine();
+        String secondLine = scanner.nextLine();
+        firstLine = firstLine.replace(" ", "");
+        secondLine = secondLine.replace(" ", "");
+        System.out.println(firstLine.equals(secondLine));
+    }
+}
