81e89a1d8e88a039fd23fc8014a93870105662c0
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 16:04:59 +0530

    Complete problem "The sum of elements".

---
+++ Problems/The sum of elements/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalSum = 0;
+        int currentNum = scanner.nextInt();
+        while (currentNum != 0) {
+            totalSum = totalSum + currentNum;
+            currentNum = scanner.nextInt();
+        }
+        System.out.println(totalSum);
+    }
+}
