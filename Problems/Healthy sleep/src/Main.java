8b73b8492917dad5dac2dcfd58ae49545a85c2cb
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 12:58:13 +0530

    complete problem "Healthy Sleep"

---
+++ Problems/Healthy sleep/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int recommended = scanner.nextInt();
+        int excess = scanner.nextInt();
+        int dailySleepHours = scanner.nextInt();
+        if (dailySleepHours < recommended) {
+            System.out.println("Deficiency");
+        } else if (dailySleepHours >= recommended && dailySleepHours <= excess) {
+            System.out.println("Normal");
+        } else {
+            System.out.println("Excess");
+        }
+    }
+}
