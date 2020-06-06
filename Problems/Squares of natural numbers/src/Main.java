4fa9dc9e82530fb8f0a103b4c70983da8902bae7
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-06 16:30:03 +0530

    Complete problem "Squares of natural numbers".

---
+++ Problems/Squares of natural numbers/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int limitNumber = scanner.nextInt();
+        int i = 1;
+        while (i * i <= limitNumber) {
+            long number = i * i;
+            System.out.println(number);
+            ++i;
+        }
+    }
+}
