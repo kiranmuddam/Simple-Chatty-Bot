ca935f7cae6f89dabe13f8e9475310677b7a3d92
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-07 10:34:24 +0530

    Complete problem "Direction".

---
+++ Problems/Direction/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        switch (userInput) {
+            case 0:
+                System.out.println("do not move");
+                break;
+            case 1:
+                System.out.println("move up");
+                break;
+            case 2:
+                System.out.println("move down");
+                break;
+            case 3:
+                System.out.println("move left");
+                break;
+            case 4:
+                System.out.println("move right");
+                break;
+            default:
+                System.out.println("error!");
+                break;
+        }
+    }
+}
